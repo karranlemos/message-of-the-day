b1f1b6b40974436118c24cd0c715b08acf05d2aa
Karran Lemos <karranlemos@gmail.com>
2020-09-11 12:29:56 -0300

    Add home page; add css; add Noto Sans

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const api = require('./api/index');
 const pages = require('./pages/index');
 
+router.use(express.static('public'));
+
 router.use('/api', api);
 router.use('/', pages);
 
