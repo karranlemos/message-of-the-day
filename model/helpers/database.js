3bcda5f2521f5b53d52e3c31b994e4cbd3e1ecdf
Karran Lemos <karranlemos@gmail.com>
2020-09-11 22:51:50 -0300

    Allow database credentials to be provided through env variables

--- model/helpers/database.js
+++ model/helpers/database.js
@@ -1,17 +1,13 @@
 const mysql = require('mysql');
-
-const dbValues = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'message_of_the_day'
-};
+const url = require('url');
 
 var dbSingleton;
 
+var dbValues;
+
 class Database {    
     constructor() {        
-        this.connection = mysql.createConnection(dbValues);
+        this.connection = mysql.createConnection(Database.getCredentials());
         this.connection.connect(err => {
             if (err)
                 throw 'Could not connect to the database...';
@@ -27,10 +23,39 @@ class Database {
         this.connection.query(sql, params, callback);
     }
 
+    
+    
     static getInstance() {
         if (!dbSingleton)
             dbSingleton = new Database();
         return dbSingleton;
+    }
+
+    static getCredentials() {
+        if (!dbValues)
+            dbValues = Database.loadCredentials();
+        return dbValues;
+    }
+
+    static loadCredentials() {
+        const defaultCredentials = {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'message_of_the_day'
+        }
+
+        if (!process.env.CLEARDB_DATABASE_URL)
+            return defaultCredentials;
+        
+        const urlCredentials = new url.URL(process.env.CLEARDB_DATABASE_URL);
+
+        return {
+            host: urlCredentials.host,
+            user: urlCredentials.username,
+            password: urlCredentials.password,
+            database: urlCredentials.pathname.substr(1)
+        };
     }
 }
 
